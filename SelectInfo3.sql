3ed0d963dfbd1e696bc69e4bef88b09c96d4cf9e
CHN-STUDENT <CHN-STUDENT@outlook.com>
2018-10-14 20:34:59 +0800

    Add files via upload

--- SelectInfo3.sql
+++ SelectInfo3.sql
@@ -6,9 +6,9 @@ USE SCT; --不要忘了这句，要不然可能查不到
 --一定要谨慎！！！
 --记住！！！
 --（1）	检索有薪水差额的任意两位教师的姓名及二者的工资差额；
-    Select DISTINCT T1.Tname, T2.Tname
+    Select DISTINCT T1.Tname, T2.Tname,( T1.Salary - T2.Salary) AS [Difference]
     From Teacher AS T1, Teacher AS T2
-    Where T1.Salary > T2.Salary; 
+    Where T1.Salary != T2.Salary; 
 --（2）	检索“001”号课成绩比“002”号课成绩高的所有学生的姓名、001号课程成绩、002号课程成绩及成绩差；
 	SELECT DISTINCT Student.Sname,
 		SC1.Score AS [Score(001)],
@@ -99,9 +99,17 @@ USE SCT; --不要忘了这句，要不然可能查不到
 		WHERE S#='98030101'
 			  AND SC2.C#=SC1.C#
 	);
+/*
+	！！！所有涉及更改，插入，新建新表以及删除操作，都应该在第二次执行之前将结果重置为默认值，不然将会无法检验结果正确性。！！！
+	！！！所有涉及增删改操作测试都应该在测试之前将所有已经更改（增加或者删除）的结果重置！！！
+	！！！否则之前结果会影响第二次执行结果！！！
+	！！！否则之前结果会影响第二次执行结果！！！
+	！！！否则之前结果会影响第二次执行结果！！！
+	！！！应该在第一次测试后前就重置，而不是第二次开始前重置，否则会同样影响结果！！！
+	！！！应该在第一次测试后前就重置，而不是第二次开始前重置，否则会同样影响结果！！！
+	！！！应该在第一次测试后前就重置，而不是第二次开始前重置，否则会同样影响结果！！！
+*/
 --（10）	新建Table: SCt(S#, C#, Score), 将检索到的成绩不及格同学的记录新增到该表中；
-	--删除之前可能存在的已经创建的表
-	DROP TABLE SCt;
 	--执行查询并且添加新表任务
 	SELECT S#,C#,Score
 	INTO SCt --直接将结果插入新表
@@ -119,14 +127,23 @@ USE SCT; --不要忘了这句，要不然可能查不到
 					HAVING COUNT(*) =2);
 	--检索删除后的新表
 	SELECT * FROM SCt;
+	--做增删改测试之后应该将更改（增加，删除）的值予以重置，否则将会影响第二次测试结果！！！
+/*
+	！！！所有涉及更改，插入，新建新表以及删除操作，都应该在第二次执行之前将结果重置为默认值，不然将会无法检验结果正确性。！！！
+	！！！所有涉及增删改操作测试都应该在测试之前将所有已经更改（增加或者删除）的结果重置！！！
+	！！！否则之前结果会影响第二次执行结果！！！
+	！！！否则之前结果会影响第二次执行结果！！！
+	！！！否则之前结果会影响第二次执行结果！！！
+	！！！应该在第一次测试后前就重置，而不是第二次开始前重置，否则会同样影响结果！！！
+	！！！应该在第一次测试后前就重置，而不是第二次开始前重置，否则会同样影响结果！！！
+	！！！应该在第一次测试后前就重置，而不是第二次开始前重置，否则会同样影响结果！！！
+*/
+	DROP TABLE SCt;
 --（12）	将张三同学001号课的成绩置为其班级该门课的平均成绩。
--- 这题我没看懂什么意思，但是我可以做到将所有班级，所有的课程的ID查询出来，并且把平均成绩也算出来添加到一个新表到SCa(Sclass, C#, AVG_Score)去，然后将张三同学的001号课的成绩选出来，并且做为他们班该门课的平均成绩。
-	
-	-- 删除之前可能存在的表
-	DROP TABLE SCa;
+	--题意：查询张三同学班级001号课的平均成绩并将其置为张三同学001号课的成绩
 	--首先创建这个表出来
-	SELECT Student.Sclass AS [Sclass],
-		   Course.C# AS [C#]
+	SELECT DISTINCT Student.Sclass AS [Sclass],
+			Course.C# AS [C#]
 	INTO SCa
 	FROM Student,Course;
 	--在这个表中添加AVG_Score列
@@ -135,19 +152,46 @@ USE SCT; --不要忘了这句，要不然可能查不到
 	--检索新建结果
 	SELECT * FROM SCa;
 	--计算每个班每门课的平均值插入结果到SCa表
-	INSERT INTO SCa 
-	SELECT Student.Sclass AS [Sclass],SC.C# AS [C#],AVG(SC.Score) AS [AVG_Score]
-	FROM Student,SC
-	WHERE Student.S#=SC.S#
-	GROUP BY Student.Sclass,SC.C#
-	--检索新建结果
+	UPDATE SCa SET SCa.AVG_Score = Temp.AVG_Score
+	FROM(SELECT DISTINCT Student.Sclass AS [Sclass],SC.C# AS [C#],AVG(SC.Score) AS [AVG_Score]
+		FROM Student,SC
+		WHERE Student.S#=SC.S#
+		GROUP BY Student.Sclass,SC.C#) AS Temp
+	WHERE SCa.Sclass=Temp.Sclass AND SCa.C#=Temp.C#;
+	--检索更改结果
 	SELECT * FROM SCa;
 	--将张三同学001号课的成绩置为其班级该门课的平均成绩
-	INSERT INTO SCa 
-	SELECT Student.Sclass AS [Sclass],SC.C# AS [C#],SC.Score AS [Score]
-	FROM SC,Student
-	WHERE Student.S#=SC.S#
+	--用该班该门课的平均成绩覆盖张三同学001号课的成绩
+	UPDATE SC SET SC.Score = TEMP.Score
+	--括号内查询的是张三该班001号课平均的成绩
+	--UPDATE是张三同学的001号课的成绩
+	FROM (SELECT SCa.AVG_Score AS [Score]
+		  FROM SCa,Student
+		  WHERE Student.Sclass=SCa.Sclass
+				AND Student.Sname='张三'
+				AND SCa.C#='001') AS TEMP,Student
+	WHERE Student.S#=SC.S# 
 		  AND Student.Sname='张三'
-		  AND SC.C#='001'
-	--检索新建结果
-	SELECT * FROM SCa;
+		  AND SC.C#='001';
+	--检索更改结果
+	SELECT * FROM SC;
+/*
+	！！！所有涉及更改，插入，新建新表以及删除操作，都应该在第二次执行之前将结果重置为默认值，不然将会无法检验结果正确性。！！！
+	！！！所有涉及增删改操作测试都应该在测试之前将所有已经更改（增加或者删除）的结果重置！！！
+	！！！否则之前结果会影响第二次执行结果！！！
+	！！！否则之前结果会影响第二次执行结果！！！
+	！！！否则之前结果会影响第二次执行结果！！！
+	！！！应该在第一次测试后前就重置，而不是第二次开始前重置，否则会同样影响结果！！！
+	！！！应该在第一次测试后前就重置，而不是第二次开始前重置，否则会同样影响结果！！！
+	！！！应该在第一次测试后前就重置，而不是第二次开始前重置，否则会同样影响结果！！！
+*/
+	--重置张三同学001号课的成绩（不重置会影响第二次的的结果）
+	UPDATE SC SET SC.Score = 92
+	FROM Student
+	WHERE Student.S#=SC.S# 
+		  AND Student.Sname='张三'
+		  AND SC.C#='001';
+	--检索更改结果
+	SELECT * FROM SC;
+	-- 重置为增删改前默认的状态（不重置会影响第二次的的结果）
+	DROP TABLE SCa;
